4c836ed1c63c300962491b76aca45937c20fe1fb
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-05-31 18:54:30 -0500

    Time: 1389 ms (82.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1932-grand-slam-titles/grand-slam-titles.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+#Akhil123
+with temp as(
+select year,Wimbledon as games from Championships
+union all
+select year,Fr_open as games from Championships
+union all
+select year,US_open as games from Championships
+union all
+select year,Au_open as games from Championships)
+select player_id,player_name,grand_slams_count from
+(select games,count(*) as grand_slams_count from temp group by 1)a join Players p on a.games=p.player_id
+
