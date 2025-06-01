05e774bd52e8a2d22c65eb714eeee41ef4561dfc
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-06-01 16:50:23 -0500

    Time: 1139 ms (56.47%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1964-find-interview-candidates/find-interview-candidates.sql
+++ SQL/1964-find-interview-candidates/find-interview-candidates.sql
@@ -5,16 +5,23 @@ union all
 select contest_id,silver_medal as user_id,'silver' as medal from Contests
 union all
 select contest_id,bronze_medal as user_id,'bronze' as medal from Contests),
+
 temp2 as(
     select user_id,ct from(
     select user_id,contest_id-row_number() over(partition by user_id order by contest_id) as ct from temp) a group by user_id,ct having count(*)>=3
 ),
+
 temp3 as(
 select user_id from temp where medal='gold' group by user_id having count(*)>=3
-union all
+),
+
+temp4 as(
+select user_id from temp3
+union
 select user_id from temp2
 )
-select name,mail from Users u join temp3 t on u.user_id=t.user_id group by 1,2
+
+select name,mail from Users u join temp4 t on u.user_id=t.user_id
 
 
 
