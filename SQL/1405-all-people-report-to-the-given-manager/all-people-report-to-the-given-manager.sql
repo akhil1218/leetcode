d0baab3f61bacdef59f7a530d5b94f2cb75d4a15
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-06-02 19:23:53 -0500

    Time: 256 ms (45.65%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ SQL/1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,5 +1,18 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
 
+-- --brute force solution
+
+-- with temp as(
+-- SELECT employee_id from Employees where manager_id=1 and employee_id!=1),
+-- temp2 as(
+-- select employee_id from Employees where manager_id in (select employee_id from temp))
+-- select employee_id from temp
+-- union all
+-- select employee_id from temp2
+-- union all
+-- select employee_id from Employees where manager_id in (select employee_id from temp2)
+
+-- --recursive solution
 with recursive temp as(
     SELECT employee_id from Employees where manager_id=1 and employee_id!=1
     union all
