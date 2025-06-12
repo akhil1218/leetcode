9b86b3b043a4e4ee4cebadc42c5f9bf8d34e1832
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-06-11 20:16:02 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (31.25%) - LeetSync

--- SQL/283-move-zeroes/move-zeroes.py
+++ SQL/283-move-zeroes/move-zeroes.py
@@ -2,18 +2,9 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         i=0
         for j in range(len(nums)):
-            if j+1<len(nums):
-                if nums[j]==0:
-                    if nums[j+1]==0:
-                        pass
-                    else:
-                        t=nums[j]
-                        nums[i]=nums[j+1]
-                        nums[j+1]=t
-                        i+=1
-                else:
-                    i+=1
-                    pass
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
                 
 
 
