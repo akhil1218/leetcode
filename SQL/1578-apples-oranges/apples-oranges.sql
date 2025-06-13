c213f5c50f7f99a19b1cb19e776bf5e9d92e85bc
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-06-12 19:25:01 -0500

    Time: 316 ms (85.71%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1578-apples-oranges/apples-oranges.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select sale_date,sum(case when fruit='apples' then sold_num else 0 end)-sum(case when fruit='oranges' then sold_num else 0 end) as diff from Sales group by sale_date order by 1
