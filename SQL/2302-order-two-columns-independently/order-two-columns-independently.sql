176972011d7aad7ecaa6997e307bc5ea8693b1ef
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-09 17:34:33 -0500

    Time: 495 ms (17.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/2302-order-two-columns-independently/order-two-columns-independently.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+with first as(
+select first_col,row_number() over(order by first_col) as ordr from Data order by 1),
+second as(
+select second_col,row_number() over(order by second_col desc) as ordr from Data order by 1 desc)
+select first_col,second_col from first f join second s on f.ordr=s.ordr order by 1, 2 desc
