6a07af92c2d7278af0545ee013641f6d37ac8ada
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-11 12:28:51 -0500

    Time: 433 ms (9.58%) | Memory: 0B (100.00%) - LeetSync

--- SQL/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ SQL/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select distinct employee_id from Employees where manager_id is not null and manager_id not in(select employee_id from Employees) and salary<30000 order by 1
+select a.employee_id from(
+select employee_id,manager_id from Employees where manager_id is not null and salary<30000) a left join Employees e on a.manager_id=e.employee_id where e.employee_id is null order by 1
+
+
