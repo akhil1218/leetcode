7a17a578956441c56dbc7224d088704556b80ddb
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-13 17:20:10 -0500

    Time: 928 ms (50.46%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1415-students-and-examinations/students-and-examinations.sql
+++ SQL/1415-students-and-examinations/students-and-examinations.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
-with student_subjects as(
-select s.student_id,s.student_name,e.subject_name,count(s.student_id) as attended_exams from Students s join Examinations e on s.student_id=e.student_id group by 1,2,3),
-all_students as(
+with all_subjects as(
 select student_id,student_name,subject_name from Students,Subjects)
-select p.student_id,p.student_name,p.subject_name,coalesce(attended_exams,0) as attended_exams from all_students  p left join student_subjects q on p.student_id=q.student_id and p.subject_name=q.subject_name order by 1,3
-
+select a.student_id,a.student_name,a.subject_name,count(e.student_id) as attended_exams from all_subjects a left join Examinations e on a.student_id=e.student_id and a.subject_name=e.subject_name group by a.student_id,a.student_name,a.subject_name order by a.student_id,a.subject_name
 
 
