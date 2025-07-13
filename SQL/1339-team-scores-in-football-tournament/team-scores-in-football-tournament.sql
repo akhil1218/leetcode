e9f7ac398f91f77f735e72c3142302298f3a963e
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-12 20:23:48 -0500

    Time: 726 ms (73.39%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ SQL/1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -7,4 +7,4 @@ union all
 select guest_team as team_id,case when guest_goals>host_goals then 3
 when host_goals=guest_goals then 1
 else 0 end as num_points from Matches)
-select t1.team_id,team_name,coalesce(sum(num_points),0) as num_points from Teams t1 left join temp t2 on t1.team_id=t2.team_id group by team_id,team_name order by num_points desc,team_id
+select t1.team_id,team_name,ifnull(sum(num_points),0) as num_points from Teams t1 left join temp t2 on t1.team_id=t2.team_id group by team_id,team_name order by num_points desc,team_id
