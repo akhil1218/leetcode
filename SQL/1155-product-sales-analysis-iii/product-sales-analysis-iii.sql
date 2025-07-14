b2dced4065bf6fe1100b900bca10c0d198daa7ca
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-13 19:36:54 -0500

    Time: 722 ms (60.36%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ SQL/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 with all_sales as(
-select product_id,year,quantity,price,rank() over(partition by product_id order by year) as rnk from Sales)
+select product_id,year,quantity,price,dense_rank() over(partition by product_id order by year) as rnk from Sales)
 select product_id,year as first_year,quantity,price from all_sales where rnk=1 group by 1,2,3,4
