dd80dd8752954080ffa212587da44d9f8aac62d8
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-14 19:58:03 -0500

    Time: 534 ms (63.65%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ SQL/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-select employee_id,department_id from Employee where primary_flag='N' and employee_id not in(select employee_id from Employee where primary_flag='Y')
-union all
-select employee_id,department_id from Employee where primary_flag='Y'
+select employee_id,department_id from(
+select employee_id,department_id,row_number() over(partition by employee_id order by primary_flag) as rnk from Employee) a where rnk=1
