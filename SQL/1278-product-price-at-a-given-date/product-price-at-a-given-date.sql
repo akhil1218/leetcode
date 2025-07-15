a09506330b9eeb85bd259bb130e9d30dbc5d7df4
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-14 21:27:31 -0500

    Time: 452 ms (74.55%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ SQL/1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-with temp as(
-select p.product_id,p.new_price as price from Products p join(
-select product_id,max(change_date) as latest from Products where change_date<='2019-08-16' group by 1) a on p.product_id=a.product_id and p.change_date=a.latest),
-temp2 as(
-select distinct product_id from Products)
-select t1.product_id,coalesce(price,10) as price from temp2 t1 left join temp t2 on t1.product_id=t2.product_id
+# Write your MySQL query statement below
+select distinct a.product_id, coalesce(b.new_price,10) as price from Products a left join
+(select product_id,rank() over(partition by product_id order by change_date desc) as xrank,new_price from Products where change_date<=date('2019-08-16'))b
+on a.product_id=b.product_id and b.xrank=1
