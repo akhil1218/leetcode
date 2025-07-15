25148867d4b495bbc88767fe792568f2ee99cea6
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-14 20:00:58 -0500

    Time: 536 ms (61.99%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ SQL/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 select employee_id,department_id from(
-select employee_id,department_id,row_number() over(partition by employee_id order by primary_flag) as rnk from Employee) a where rnk=1
+select employee_id,department_id,row_number() over(partition by employee_id order by case when primary_flag='Y' then 0 else 1 end) as rnk from Employee) a where rnk=1
