ea7a29e0dd7cd70751ed5afcb4ffad79c6cd82f6
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-15 09:56:37 -0500

    Time: 896 ms (6.01%) | Memory: 0B (100.00%) - LeetSync

--- SQL/585-investments-in-2016/investments-in-2016.sql
+++ SQL/585-investments-in-2016/investments-in-2016.sql
@@ -3,6 +3,8 @@ with temp as(
 select lat,lon from Insurance group by lat,lon having count(*)>1),
 temp2 as(
 select t1.pid,t1.tiv_2015,t1.tiv_2016,t1.lat,t1.lon from Insurance t1 join Insurance t2 on t1.pid!=t2.pid and t1.tiv_2015=t2.tiv_2015 group by 1,2,3,4,5)
-select round(sum(q1.tiv_2016),2) as tiv_2016 from temp2 q1 WHERE NOT EXISTS (
-  SELECT 1 FROM temp t WHERE t.lat = q1.lat AND t.lon = q1.lon
-)
+SELECT ROUND(SUM(q1.tiv_2016), 2) AS tiv_2016
+FROM temp2 q1
+LEFT JOIN temp t 
+  ON q1.lat = t.lat AND q1.lon = t.lon
+WHERE t.lat IS NULL;
