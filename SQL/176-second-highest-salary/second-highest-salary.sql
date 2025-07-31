4c1de6192da624457918ab91a9af93976edc9b14
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-07-31 09:30:37 -0500

    Time: 675 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- SQL/176-second-highest-salary/second-highest-salary.sql
+++ SQL/176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,1 @@
-# Write your MySQL query statement below
-select max(salary) as SecondHighestSalary  from(
-select salary,dense_rank() over(order by salary desc) as rnk from Employee) a where rnk=2
+select max(salary) as SecondHighestSalary from(select salary,dense_rank() over(order by salary desc) as rnk from Employee)a where rnk=2
