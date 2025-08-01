b701ee373fa1ae25cc4c0a6a91b6ca110e4c4a82
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-08-01 08:56:17 -0500

    Time: 523 ms (75.16%) | Memory: 0B (100.00%) - LeetSync

--- SQL/180-consecutive-numbers/consecutive-numbers.sql
+++ SQL/180-consecutive-numbers/consecutive-numbers.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select distinct l1.num as ConsecutiveNums from logs l1 join logs l2 on l1.id=l2.id-1 join logs l3 on l1.id=l3.id-2 where l1.num=l2.num and l1.num=l3.num
+with temp as(
+select num,(id-row_number() over(partition by num order by id)) as rn from Logs)
+select distinct num as ConsecutiveNums from temp group by num,rn having count(*)>=3
