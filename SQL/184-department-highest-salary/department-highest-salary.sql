b4cf008830164aad12915cc52276324dbd7fea22
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-08-01 10:12:34 -0500

    Time: 544 ms (99.41%) | Memory: 0B (100.00%) - LeetSync

--- SQL/184-department-highest-salary/department-highest-salary.sql
+++ SQL/184-department-highest-salary/department-highest-salary.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 select Department,Employee,salary as Salary from(
-select d.name as Department,e.name as Employee,salary,rank() over(partition by d.name order by salary desc) as rnk from Employee e join Department d on e.departmentId=d.id) a where rnk=1
+select d.name as Department,e.name as Employee,salary,rank() over(partition by d.id order by salary desc) as rnk from Employee e join Department d on e.departmentId=d.id) a where rnk=1
