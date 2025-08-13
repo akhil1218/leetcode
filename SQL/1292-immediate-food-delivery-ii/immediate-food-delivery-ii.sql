8954816ec31bddb754cce8b4fbda257680775995
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-08-12 22:14:23 -0500

    Time: 579 ms (84.81%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ SQL/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-with first_orders as(
-select customer_id,min(order_date) as first_date,min(customer_pref_delivery_date) as cust_first_date from Delivery group by customer_id)
-select round(((count(*))/(select count(*) from first_orders))*100,2) as immediate_percentage  from first_orders where first_date=cust_first_date
+with temp as(select customer_id, order_date,customer_pref_delivery_date from(
+select customer_id, order_date,customer_pref_delivery_date,rank() over(partition by customer_id order by order_date) as rnk from Delivery)a where rnk=1)
+select round(count(distinct customer_id)*100/(select count(distinct customer_id) from Delivery),2) as immediate_percentage from temp where order_date=customer_pref_delivery_date
