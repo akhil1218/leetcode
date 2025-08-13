04dd486032cd0f27b9ed4c9ec01a89022be4506a
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-08-12 19:52:58 -0500

    Time: 239 ms (68.74%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ SQL/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-with process_details as(
-select machine_id,process_id,(sum(case when activity_type='end' then timestamp end)-sum(case when activity_type='start' then timestamp end)) as dif from Activity group by machine_id,process_id)
--- select * from process_details
-select machine_id,round((sum(dif)/count(process_id)),3) as processing_time from process_details group by machine_id
+with time_took as(
+    select machine_id,process_id,max(case when activity_type='end' then timestamp end)-max(case when activity_type='start' then timestamp end) as time_taken from Activity group by machine_id,process_id
+)
+select machine_id,round(avg(time_taken),3) as processing_time from time_took group by machine_id
