e23354463eb7208181e75c05c054fc16982f3766
akhil1218 <117544098+akhil1218@users.noreply.github.com>
2025-08-12 22:05:56 -0500

    Time: 647 ms (29.72%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ SQL/1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement below
-with players_grp as(
-select player_id,event_date,date_sub(event_date,Interval row_number() over(partition by player_id order by event_date) day) as dif from Activity),
-players_fdate as(
-select player_id,dif,min(event_date) as first_date from players_grp group by player_id,dif having count(*)>1),
-player_mindate as(
-    select player_id,min(event_date) as first_date from Activity group by 1
-)
-select round((count(*)*1.0/(select count(distinct player_id) from Activity)),2) as fraction from players_fdate pf join player_mindate pm on pf.player_id=pm.player_id and pf.first_date=pm.first_date
+with groups2 as(
+select player_id, event_date,date_sub(event_date,Interval row_number() over(partition by player_id order by event_date) day) as grp,games_played from Activity),
+inital_players as(
+select player_id from(
+select player_id,dense_rank() over(partition by player_id order by grp) as rnk from groups2)a where rnk=1 group by player_id having count(*)>1)
+select round(count(player_id)/(select count(distinct player_id) from Activity),2) as fraction from inital_players
